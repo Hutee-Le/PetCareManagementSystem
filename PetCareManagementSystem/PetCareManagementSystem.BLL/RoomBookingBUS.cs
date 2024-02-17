00db17136f47babfe0d3f834dff6d3f2e11aa2b5
Minh Nhut <lenhut0929@gmail.com>
2024-02-18 00:02:48 +0700

    feat(RoomBooking): implement retrieval of spa pet bookings
    Add method in RoomBookingBUS to fetch spa booking details from the database using RoomBookingDAO. The retrieved data is then transformed into a list of SpaPetBookingDetail view models suitable for data binding in the SpaBookingForm DataGridView.

---
+++ PetCareManagementSystem/PetCareManagementSystem.BLL/RoomBookingBUS.cs
@@ -0,0 +1,48 @@
+﻿using PetCareManagementSystem.DAL.DAO;
+using PetCareManagementSystem.DTO.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCareManagementSystem.BLL
+{
+    public class RoomBookingBUS
+    {
+        private RoomBookingDAO _dao;
+
+        public RoomBookingBUS()
+        {
+            _dao = new RoomBookingDAO();
+        }
+
+        public List<SpaPetBookingDetail> GetAllSpaPetBookings()
+        {
+            DataTable dtBookings = _dao.GetAllSpaPetBookings();
+            List< SpaPetBookingDetail> listBookings = new List<SpaPetBookingDetail>();
+
+            foreach (DataRow row in dtBookings.Rows)
+            {
+                listBookings.Add(new SpaPetBookingDetail
+                {
+                    BookingID = Convert.ToInt32(row["BookingID"]),
+                    CustomerName = row["Name"].ToString(),
+                    Date = Convert.ToDateTime(row["Date"]),
+                    ServiceName = row["ServiceName"].ToString(),
+                    TotalPrice = Convert.ToDecimal(row["TotalPrice"]),
+                    EmployeeName = row["EmployeesName"].ToString(),
+                    PetName = row["PetsName"].ToString(),
+                    BreedName = row["BreedName"].ToString(),
+                    SpeciesName = row["SpeciesName"].ToString(),
+                    Status = row["Status"].ToString(),
+                    PaymentStatus = row["PaymentStatus"].ToString(),
+                    TypeName = row["TypeName"].ToString(),
+                });
+            }
+
+            return listBookings;
+        }
+    }
+}
---
+++ PetCareManagementSystem/PetCareManagementSystem.DTO/ViewModels/SpaPetBookingDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetCareManagementSystem.DTO.ViewModels
+{
+    public class SpaPetBookingDetail
+    {
+        public int BookingID { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime Date { get; set; }
+        public string ServiceName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string EmployeeName { get; set; }
+        public string PetName { get; set; }
+        public string BreedName { get; set; }
+        public string SpeciesName { get; set; }
+        public string Status { get; set; }
+        public string PaymentStatus { get; set; }
+        public string TypeName { get; set; }
+
+        public string PetInformation
+        {
+            get
+            {
+                return $"{SpeciesName} ({BreedName})";
+            }
+        }
+    }
+}
